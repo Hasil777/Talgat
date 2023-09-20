cbf3a03959d6d66b49b429b5cda9389cf2317e87
Hasil777 <hasilbek2008@gmail.com>
2023-09-20 17:36:13 +0500

    first commit

--- config/settings.py
+++ config/settings.py
@@ -27,12 +27,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post',
+    'django_filters',
     'rest_framework',
     'corsheaders',
-
-    'django_filters',
-
-    'post',
 ]
 
 MIDDLEWARE = [
